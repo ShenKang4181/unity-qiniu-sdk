c330f02093eb2c6e9c558b5a0db02a217afb3f8a
ShenKang4181 <1284784181@qq.com>
2022-07-02 18:00:58 +0800

    Update QiNiu.cs

--- Qiniu/QiNiu.cs
+++ Qiniu/QiNiu.cs
@@ -3,17 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Qiniu.IO;
 
-public class QiNiu : MonoBehaviour
+public static class QiNiu 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
